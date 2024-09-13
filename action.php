acd504d85492b4e5a0469b5b8b3b982eb69bf77d
parthhh27 <shrikrushna2265@gmail.com>
2024-09-13 18:33:14 +0530

    Update action.php

--- action.php
+++ action.php
@@ -7,7 +7,7 @@ $ren = new mysqli('localhost','root','','test');
 if($ren->connect_error){
     die('error :(');
 }else{
-    $notren = $con->prepare("insert into registration'TIME','Fullname','email','feedback')
+    $notren = $con->prepare("insert into sheet'TIME','Fullname','email','feedback')
     values(?,?,?,?)");
 }
 $notren->bind_param("isss",$TIME,$Fullname,$email,$feedback);
