f16efeef1dab4d78dd0b2681099ff6ef48580180
parthhh27 <shrikrushna2265@gmail.com>
2024-09-17 08:06:48 +0530

    noerror action.php

--- action.php
+++ action.php
@@ -1,18 +1,23 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 $fullname = $_POST['fullname'];
 $email = $_POST['email'];
-$feedback = $_POST['feed'];
+$feedback = $_POST['feedback'];
 
-$ren = new mysqli('localhost','root','','test');
-if($ren->connect_error){
-    die('error :(');
-}else{
-    $notren = $ren->prepare("insert into sheet'TIME','Fullname','email','feedback')
-    values(?,?,?,?)");
+echo ("my name is "),"$fullname<br>";
+echo ("my email is "),"$email<br>";
+echo ("my feedback is "),"$feedback<br>";
+
+$play = new mysqli('localhost', 'root', '', 'forform');
+
+if ($play->connect_error) {
+    die("Connection failed: " . $play->connect_error);
 }
-$notren->bind_param("isss",$TIME,$Fullname,$email,$feedback);
-$notren->execute();
-echo"thanks";
-$notren->close();
-$ren->close();
-?>
+$nav = $play->prepare("INSERT INTO seva(fullname, email, feedback) VALUES (?, ?, ?)");
+$nav->bind_param("sss", $fullname, $email, $feedback);
+$nav->execute();
+$nav->close();
+$play->close();
