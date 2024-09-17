4ec687f5094e854190204896d31e3fc6c665e2f7
parthhh27 <shrikrushna2265@gmail.com>
2024-09-17 08:07:44 +0530

    Update initial.php

--- initial.php
+++ initial.php
@@ -5,16 +5,17 @@
     <body><br>
         <form action="action.php" method="POST">
             <label for="fullname">Enter your full name: </label>
-            <input type="text" id="fullname" required placeholder="Param Pujja" name="name"><br>
+            <input type="text" id="fullname" required placeholder="Param Pujja" name="fullname"><br>
             
             <label for="email">Enter your email: </label>
             <input type="text" id="email" required placeholder="parampujja@gmail.com" name="email"><br>
 
             Please give your feedback <br>
-            <textarea id="feedback" required rows="5" cols="30" name="feed"></textarea>
+            <textarea id="feedback" required rows="5" cols="30" name="feedback"></textarea><br>
             
-            <input type="reset"><br>
+            <input type="reset"><br> 
             <input type="submit"><br>
+        
             
 
 
