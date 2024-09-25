5ad597d6b3bb52ec0188b1f975a79fb150a21cf8
parthhh27 <shrikrushna2265@gmail.com>
2024-09-25 10:31:48 +0530

    $time is working, new file created(feedback.txt)

--- action.php
+++ action.php
@@ -1,23 +1,25 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 1); //to show up any error right at the start
 
 $fullname = $_POST['fullname'];
 $email = $_POST['email'];
 $feedback = $_POST['feedback'];
+$time=date('y-m-d h:i:s');
+$line="time: $time, name: $fullname, email: $email, feedback: $feedback";
+$file="feedback.txt";
+file_put_contents($file, $line, FILE_APPEND | LOCK_EX);//used cmd command sudo chmod -r 777 feedback.txt
 
-echo ("my name is "),"$fullname<br>";
-echo ("my email is "),"$email<br>";
-echo ("my feedback is "),"$feedback<br>";
+echo "Thank you for your feedback ", "$fullname<br>";
 
-$play = new mysqli('localhost', 'root', '', 'forform');
+$play = new mysqli('localhost', 'root', '', database: 'forform');
 
 if ($play->connect_error) {
-    die("Connection failed: " . $play->connect_error);
+    die("Connection failed: " . $play->connect_error);//did not display die error message
 }
-$nav = $play->prepare("INSERT INTO seva(fullname, email, feedback) VALUES (?, ?, ?)");
-$nav->bind_param("sss", $fullname, $email, $feedback);
+$nav = $play->prepare("INSERT INTO seva(fullname, email, feedback) VALUES (?, ?, ?)");//QUERY COMMANDS ARE CASE SENSITIVE
+$nav->bind_param("sss", $fullname, $email, $feedback);//dont know what sss is
 $nav->execute();
 $nav->close();
 $play->close();
